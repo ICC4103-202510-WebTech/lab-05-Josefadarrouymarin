8a46d5bcc73e687d98af43a773c65a56c8107f64
mikaelaguerrero <miguerrero@miuandes.cl>
2025-04-18 19:28:55 -0400

    Lab 6 terminado

---
+++ db/seeds.rb
@@ -0,0 +1,27 @@
+puts "Creating users..."
+users = 10.times.map do |i|
+  User.create!(
+    email: "user#{i}@example.com",
+    first_name: "First#{i}",
+    last_name: "Last#{i}"
+  )
+end
+
+puts "Creating chats..."
+chats = 10.times.map do
+  Chat.create!(
+    sender_id: users.sample.id,
+    receiver_id: users.sample.id
+  )
+end
+
+puts "Creating messages..."
+10.times do
+  Message.create!(
+    chat_id: chats.sample.id,
+    user_id: users.sample.id,
+    body: "Test message at #{Time.now}"
+  )
+end
+
+puts "Done seeding!"
